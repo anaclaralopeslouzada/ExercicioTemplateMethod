4559bef7d82875c1f19aa7e5416e23725be43354
Ana Clara <ana.louzada@unifesp.br>
2026-06-19 15:19:22 -0300

    classe program com array.sort()

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExercicioTemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = {"Banana", "Uva", "Carambola", "Maça", "Melancia"};
+            
+            Console.WriteLine("Lista Original");
+            Exibir(palavras);
+
+            Console.WriteLine("\n Ordenado por Tamanho");
+            Array.Sort(palavras, new ComparadorTamanho());
+            Exibir(palavras);
+
+            Console.WriteLine("\n Ordenado por Ultima Letra");
+            Array.Sort(palavras, new ComparadorUltimaLetra());
+            Exibir(palavras);
+        }
+
+        static void Exibir(string[] array)
+        {
+            foreach (var palavra in array)
+            {
+                Console.WriteLine(palavra + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
